0d53d4669167dab6379654abed4e4e097bebd124
NachoGoro <nachogoro.dev@gmail.com>
2023-11-20 21:13:55 +0100

    Clarify variable name

--- examples/encrypt_elgamal.py
+++ examples/encrypt_elgamal.py
@@ -3,15 +3,15 @@ Example file to encrypt a message in the Spanish alphabet using ElGamal
 """
 from cryptouned import elgamal
 
-alberto = elgamal.ElGamal_Agent(
+alicia = elgamal.ElGamal_Agent(
     name='Alicia',
     private_key=28236)
 
-bono = elgamal.ElGamal_Agent(
+bob = elgamal.ElGamal_Agent(
     name='Bob',
     private_key=21702)
 
 msg_pair = elgamal.encrypt(
-    'UNED', sender=alberto, receiver=bono, p=15485863, generator=7,
+    'UNED', sender=alicia, receiver=bob, p=15485863, generator=7,
     v=480, base=27, explain=True)
 
